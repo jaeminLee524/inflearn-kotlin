2e5159c9e30d24fef8b262a89c684a78c08331d8
이재민 <jaemin@MacBookPro.local>
2023-10-09 15:17:21 +0900

    코틀린 사칙연산 계산기 나눗셈 테스트

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -4,6 +4,10 @@ fun main() {
     val calculatorTest = CalculatorTest()
 
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -45,5 +49,37 @@ class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalArgumentException()
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalArgumentException("메시지가 다릅니다.")
+            }
+            return
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+
+        throw IllegalArgumentException("기대하는 예외가 발생하지 않았습니다.")
     }
 }
