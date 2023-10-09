2db257fed65183629073d414b9a4c5703f53fee5
이재민 <rkwoals524@hanmail.net>
2023-10-09 17:08:04 +0900

    코틀린 유저 service layer 테스트

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,25 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+m
+=src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt,2/6/26bd4164d30442ed097f95a5c18582c33742962c
+q
+Asrc/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt,b/3/b30eacff86bed905e8b2355a85d1a0cdeef5369c
+l
+<src/test/kotlin/com/group/libraryapp/calculator/JunitTest.kt,9/5/95a3634d048448a5913c1267c15f21de90733430
+v
+Fsrc/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt,f/1/f10f4e686fdc2b8cdb621848c7240dcf27bbf3b7
+p
+@src/main/java/com/group/libraryapp/service/user/UserService.java,1/6/16855b605ec06fc203113247571d3282b612f54b
+z
+Jsrc/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java,a/6/a641979f548b9ae8906857cf67ac91eab72344c5
+h
+8src/main/java/com/group/libraryapp/domain/user/User.java,c/0/c0c8957e937fff6691ac488aeaaf1c0d697fe4b7
+t
+Dsrc/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt,3/7/37d3f23745eaa2a80cba26edb1afe42cf7118ae4
+v
+Fsrc/main/java/com/group/libraryapp/controller/user/UserController.java,0/b/0b98a6aa905dc4fa3f632faba0caadfe9cab678d
--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,12 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.tuple
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +17,11 @@ class UserServiceTest @Autowired constructor(
     private val userService: UserService,
     private val userRepository: UserRepository,
 ) {
+
+    @AfterEach
+    fun tearDown() {
+        userRepository.deleteAllInBatch()
+    }
 
     @DisplayName("유저 정보를 저장할 수 있다.")
     @Test
@@ -28,5 +37,55 @@ class UserServiceTest @Autowired constructor(
         val users = userRepository.findAll()
         assertThat(users[0].name).isEqualTo(name)
         assertThat(users[0].age).isNull()
+    }
+
+    @DisplayName("전체 유저 정보를 조회할 수 있습니다.")
+    @Test
+    fun getUsers() {
+        // given
+        userRepository.saveAll(
+            listOf(
+                User("이재민", 28),
+                User("홍길동", null),
+            )
+        )
+
+        // when
+        val users = userService.getUsers()
+
+        // then
+        assertThat(users).hasSize(2).extracting("name", "age")
+            .containsExactlyInAnyOrder(
+                tuple("이재민", 28),
+                tuple("홍길동", null),
+            )
+    }
+
+    @DisplayName("회원의 이름 정보를 수정할 수 있습니다.")
+    @Test
+    fun updateUserName() {
+        // given
+        val savedUser = userRepository.save(User("이재민", 28))
+        val userUpdateRequest = UserUpdateRequest(savedUser.id, "홍길동")
+
+        // when
+        userService.updateUserName(userUpdateRequest)
+
+        // then
+        val findUser = userRepository.findAll()[0]
+        assertThat(findUser.name).isEqualTo("홍길동")
+    }
+
+    @DisplayName("회원정보를 삭제할 수 있습니다.")
+    @Test
+    fun deleteUser() {
+        // given
+        userRepository.save(User("이재민", null))
+
+        // when
+        userService.deleteUser("이재민")
+
+        // then
+        assertThat( userRepository.findAll()).isEmpty()
     }
 }
